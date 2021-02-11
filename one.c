4c606af4a8416fcd3cfff99a127c2a0ecb99134d
Your Name <you@example.com>
2021-02-11 21:33:47 +0530

    hii

---
+++ one.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+ int x,y ;
+ printf("Enter A Number:");
+ scanf("%d",&x);
+ printf("Enter Second Number:");
+ scanf("%d",&y);
+ printf("Sum of these numbers is=%d",x+y);
+}
